d4a5de33279065107873b206d13461debece4cc9
lixiao <932184220@qq.com>
2024-09-23 13:26:11 +0800

    feat: 塘口编辑收费管理

--- pages/pond/detail/detail.js
+++ pages/pond/detail/detail.js
@@ -1,7 +1,7 @@
 // pages/pond/detail/detail.js
-import { getPublicFishPond, postPrivateFishAdminPondAdd } from "@/api/index";
-import { env } from "@/utils/env";
-import { mainBehavior } from "@/store/behaviors";
+import { getPublicFishPond, postPrivateFishAdminPondAdd } from "@/api/index"
+import { env } from "@/utils/env"
+import { mainBehavior } from "@/store/behaviors"
 
 Page({
   behaviors: [mainBehavior],
@@ -10,77 +10,125 @@ Page({
    */
   data: {
     form: {
-      status: 1
+      status: 1,
     },
-    fileList: []
+    fileList: [],
+    standard: 1,
+    rules: [
+      {
+        fish: "",
+        price: "",
+        duration: "",
+      },
+    ],
+    originRules: [{ fish: "", price: "", duration: "" }],
   },
 
   async getData(id) {
-    const { data } = await getPublicFishPond({ id });
-    const { files = [], ...form } = data;
-    const fileList = files.map(v => ({ id: v.id, url: v.path }));
+    const { data } = await getPublicFishPond({ id })
+    const { files = [], ...form } = data
+    const fileList = files.map((v) => ({ id: v.id, url: v.path }))
     this.setData({
       form,
-      fileList
-    });
+      fileList,
+      standard: data.charge_rule?.[0]?.fish?.length > 0 ? 2 : 1,
+      rules: data.charge_rule || [{ fish: "", price: "", duration: "" }],
+      originRules: data.charge_rule || [{ fish: "", price: "", duration: "" }],
+    })
   },
   handleChange(event) {
-    const { field } = event.currentTarget.dataset;
-    const value = event.detail;
-    this.setData({ [`form.${field}`]: value });
+    const { field } = event.currentTarget.dataset
+    const value = event.detail
+    this.setData({ [`form.${field}`]: value })
+  },
+  handleRuleChange(event) {
+    const value = event.detail
+    if (value === 1) {
+      this.setData({ rules: [{ fish: "", price: "", duration: "" }] })
+    } else {
+      this.setData({ rules: [...this.data.originRules] })
+    }
+    this.setData({ standard: value })
+  },
+  handleRuleField(event) {
+    const { field, index } = event.currentTarget.dataset
+    const value = event.detail
+    this.data.rules[index][field] = value
+    this.setData({ rules: this.data.rules })
+  },
+  handleRuleDelete(event) {
+    console.log(event);
+    const { index } = event.currentTarget.dataset
+    this.data.rules.splice(index, 1)
+    this.setData({ rules: this.data.rules })
   },
   afterRead(event) {
-    const { file } = event.detail;
-    const { fileList } = this.data;
-    const that = this;
+    const { file } = event.detail
+    const { fileList } = this.data
+    const that = this
     wx.uploadFile({
       url: env.baseURL + "/private/fish/admin/photo/add",
       filePath: file.url,
       name: "file",
       header: { "x-token": wx.getStorageSync("token") },
       success(res) {
-        const { data, code } = JSON.parse(res.data);
+        const { data, code } = JSON.parse(res.data)
         if (code != 200) {
           wx.showToast({
             title: "上传失败请重试",
-            icon: "none"
-          });
-          return;
+            icon: "none",
+          })
+          return
         }
         // 上传完成需要更新 fileList
-        fileList.push({ id: data.id, url: data.url });
-        that.setData({ fileList });
-      }
-    });
+        fileList.push({ id: data.id, url: data.url })
+        that.setData({ fileList })
+      },
+    })
+  },
+  addNewRule() {
+    this.data.rules.push({
+      fish: "",
+      price: "",
+      duration: "",
+    })
+    this.setData({ rules: this.data.rules })
   },
   async handleSave() {
-    const { groupId, form, fileList } = this.data;
-    const photo_ids = fileList.map(v => v.id);
+    const { groupId, form, fileList } = this.data
+    const photo_ids = fileList.map((v) => v.id)
+    if (this.data.standard === 1) {
+      this.data.rules.forEach((item) => {
+        delete item.fish
+      })
+      this.setData({ rules: this.data.rules })
+    }
     const params = {
       angling_site_id: groupId,
       ...form,
       photo_ids,
       size: +form.size,
-      position_num: +form.position_num
+      position_num: +form.position_num,
+      charge_rule: this.data.rules,
       // water_depth: +form.water_depth
-    };
-    await postPrivateFishAdminPondAdd(params);
+    }
+    await postPrivateFishAdminPondAdd(params)
     wx.showToast({
       title: form.id ? "修改成功" : "新增成功",
       success() {
         setTimeout(() => {
-          wx.navigateBack();
-        }, 1000);
-      }
-    });
+          wx.navigateBack()
+        }, 1000)
+      },
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    wx.setNavigationBarTitle({ title: options.id ? "修改塘口" : "新增塘口" });
-    options.id && this.getData(options.id);
+    wx.setNavigationBarTitle({ title: options.id ? "修改塘口" : "新增塘口" })
+    options.id && this.getData(options.id)
   },
 
   /**
@@ -116,5 +164,5 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {}
-});
+  onShareAppMessage() {},
+})

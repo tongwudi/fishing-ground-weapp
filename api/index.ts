508ed320bae0134b8a66ab6251bf12a566d5df32
wangzhongjie <uvdream@163.com>
2024-09-23 09:18:55 +0800

    fix: 接口更改

--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,19 @@
+/*
+ * @Author: wangzhongjie
+ * @Date: 2024-09-20 17:02:31
+ * @LastEditors: wangzhongjie
+ * @LastEditTime: 2024-09-23 09:16:25
+ * @Description: 
+ * @Email: shutdown0630@163.com
+ */
 // @ts-ignore
 /* eslint-disable */
 // API 更新时间：
 // API 唯一标识：
-import * as yonghu from './yonghu';
-import * as qita from './qita';
-import * as fishAdmin from './fishAdmin';
-import * as v2Admin from './v2Admin';
-import * as ceshijiekou from './ceshijiekou';
-import * as fish from './fish';
-export default {
-  yonghu,
-  qita,
-  fishAdmin,
-  v2Admin,
-  ceshijiekou,
-  fish,
-};
+export * from './yonghu';
+export *  from './qita';
+export * from './fishAdmin';
+export *  from './v2Admin';
+export *  from './ceshijiekou';
+export *  from './fish';
+
